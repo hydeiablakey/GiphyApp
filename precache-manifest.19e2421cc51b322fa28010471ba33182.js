23c2e660ac6e206b0859f8513535b9a2921ed87c
Hydeia <blakeyhydeia@gmail.com>
2019-05-23 21:25:40 -0400

    Updates

---
+++ precache-manifest.19e2421cc51b322fa28010471ba33182.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72489121bc2202ee2ed1e1ac5a02822",
+    "url": "/GiphyApp/index.html"
+  },
+  {
+    "revision": "4c4f05880695558e407d",
+    "url": "/GiphyApp/static/css/main.9fe86986.chunk.css"
+  },
+  {
+    "revision": "23be869d6d06ef2f913d",
+    "url": "/GiphyApp/static/js/2.76a3cfbb.chunk.js"
+  },
+  {
+    "revision": "4c4f05880695558e407d",
+    "url": "/GiphyApp/static/js/main.4c735bce.chunk.js"
+  },
+  {
+    "revision": "b0af21411b34bcde063f",
+    "url": "/GiphyApp/static/js/runtime~main.9097baa5.js"
+  }
+]);
